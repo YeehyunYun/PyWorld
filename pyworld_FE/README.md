d424b89a31023b5d9e5c51d73e29c0110c1121c9
yhyun <yhyun@henergy.xyz>
2024-03-07 17:02:05 +0900

    [FE] add header, footer

--- pyworld_FE/README.md
+++ pyworld_FE/README.md
@@ -1,24 +1,23 @@
-# pyworld
+## 개발환경
+### Vue-CLI 
+vue 개발 환경을 설정해주는 도구
+```
+# 설치
+npm install -g @vue/cli
 
-## Project setup
-```
-yarn install
-```
+# 생성
+vue create (이름)
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+cd (이름)
 
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
+# 실행
+npm run serve
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## 프로젝트 구조
+### src/
+- assets/ : 이미지 등 어플리케이션에서 사용되는 파일들이 모여 있는 디렉토리
+- components/ : Vue 컴포넌트들이 모여 있는 디렉토리
+- router/ : vue-router 설정을 하는 디렉토리
+- App.vue : 가장 최상위 컴포넌트
+- main.js : 가장 먼저 실행되는 javascript 파일, Vue 인스턴스를 생성하는 역할
--- pyworld_FE/src/App.vue
+++ pyworld_FE/src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- 추가된 헤더 컴포넌트 사용 -->
+    <CommonHeader />
+    
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	
+    <!-- 추가된 풋터 컴포넌트 사용 -->
+    <CommonFooter />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CommonHeader from './components/common/CommonHeader'; //import 헤더 추가
+import HelloWorld from './components/HelloWorld.vue'; 
+import CommonFooter from './components/common/CommonFooter'; //import 풋터 추가
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CommonHeader          //헤더 컴포넌트 추가
+    , HelloWorld
+    ,CommonFooter         //풋터 컴포넌트 추가
+    
   }
 }
 </script>
 
 <style>
+html,body{padding:0; margin:0;}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin:0; padding:0;
 }
 </style>
